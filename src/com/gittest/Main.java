a001da3187920c947ddc30539ca3bcf16f3928d0
whzhou <yihangzhou2001@yahoo.com>
2016-09-03 19:16:24 -0500

    new changes

--- src/com/gittest/Main.java
+++ src/com/gittest/Main.java
@@ -6,10 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello");
         System.exit(1);
-        //new line
-        //branch code start here
-        //some more branch code change
 
-        //new changes
+
     }
 }
